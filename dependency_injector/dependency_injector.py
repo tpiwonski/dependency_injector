fe1173d310ba64d8fa777aea724013df486a49c8
tpiwonski <tpiwonski@users.noreply.github.com>
2021-03-02 23:04:05 +0100

    Add initial typing hints

--- dependency_injector/dependency_injector.py
+++ dependency_injector/dependency_injector.py
@@ -1,33 +1,40 @@
+from __future__ import annotations
+
 import functools
 from abc import ABC
-from dataclasses import MISSING, fields, is_dataclass
+from dataclasses import MISSING, dataclass, field, fields, is_dataclass
 from inspect import isclass, signature
+from typing import Any, Callable, Dict, List, Optional
+
+C = Any
+I = Any
+O = Any
 
 
-class Container(object):
-    def __init__(self):
-        self.scoped_classes = {}
-        self.transient_classes = {}
-        self.singleton_classes = {}
-        self.singleton_instances = {}
+@dataclass
+class Container:
+    scoped_classes: Dict[I, C] = field(default_factory=dict)
+    transient_classes: Dict[I, C] = field(default_factory=dict)
+    singleton_instances: Dict[I, C] = field(default_factory=dict)
+    singleton_classes: Dict[I, C] = field(default_factory=dict)
 
-    def add_singleton_class(self, interface, clazz):
+    def add_singleton_class(self, interface: I, clazz: C) -> None:
         assert issubclass(clazz, interface)
         self.singleton_classes[interface] = clazz
 
-    def add_singleton_instance(self, interface, instance):
+    def add_singleton_instance(self, interface: I, instance: O) -> None:
         assert isinstance(instance, interface)
         self.singleton_instances[interface] = instance
 
-    def add_scoped_class(self, interface, clazz):
+    def add_scoped_class(self, interface: I, clazz: C) -> None:
         assert issubclass(clazz, interface)
         self.scoped_classes[interface] = clazz
 
-    def add_transient_class(self, interface, clazz):
+    def add_transient_class(self, interface: I, clazz: C) -> None:
         assert issubclass(clazz, interface)
         self.transient_classes[interface] = clazz
 
-    def create_instance_of_interface(self, interface, scope):
+    def create_instance_of_interface(self, interface: I, scope: Scope) -> O:
         instance = self.singleton_instances.get(interface)
         if instance:
             return instance
@@ -55,27 +62,31 @@ class Container(object):
 
         raise Exception("Interface {0} not found".format(interface))
 
-    def create_instance_of_class(self, clazz, scope):
+    def create_instance_of_class(self, clazz: C, scope: Scope) -> O:
         if is_dataclass(clazz):
             params = {}
-            for field in fields(clazz):
-                if field.default is not MISSING or field.default_factory is not MISSING:
+            for class_field in fields(clazz):
+                if (class_field.default is not MISSING) or (
+                    class_field.default_factory is not MISSING  # type: ignore
+                ):
                     continue
 
                 try:
-                    if not issubclass(field.type, ABC):
+                    if not issubclass(class_field.type, ABC):
                         continue
                 except TypeError:
                     continue
 
-                instance = self.create_instance_of_interface(field.type, scope)
-                params[field.name] = instance
+                instance = self.create_instance_of_interface(class_field.type, scope)
+                params[class_field.name] = instance
 
             return clazz(**params)
         else:
             return clazz()
 
-    def create_parameters(self, func, scope, interfaces):
+    def create_parameters(
+        self, func: Any, scope: Scope, interfaces: List[I]
+    ) -> Dict[str, O]:
         params = {}
         sig = signature(func)
         for name, parameter in sig.parameters.items():
@@ -102,21 +113,22 @@ class Container(object):
         return params
 
 
-class Scope(object):
-    def __init__(self):
-        self.scoped_instances = {}
+class Scope:
+    scoped_instances: Dict[I, O] = field(default_factory=dict)
 
-    def add_instance(self, interface, instance):
+    def add_instance(self, interface: I, instance: O) -> None:
         self.scoped_instances[interface] = instance
 
-    def get_instance(self, interface):
+    def get_instance(self, interface: I) -> O:
         return self.scoped_instances.get(interface)
 
 
 _container = Container()
 
 
-def scoped(interfaces=None, container=None):
+def scoped(
+    interfaces: Optional[List[I]] = None, container: Optional[Container] = None
+) -> Any:
     return class_decorator(
         interfaces,
         container,
@@ -124,7 +136,9 @@ def scoped(interfaces=None, container=None):
     )
 
 
-def transient(interfaces=None, container=None):
+def transient(
+    interfaces: Optional[List[I]] = None, container: Optional[Container] = None
+) -> Any:
     return class_decorator(
         interfaces,
         container,
@@ -132,7 +146,7 @@ def transient(interfaces=None, container=None):
     )
 
 
-def singleton(interfaces=None, container=None):
+def singleton(interfaces=None, container=None) -> Any:
     return class_decorator(
         interfaces,
         container,
@@ -140,11 +154,13 @@ def singleton(interfaces=None, container=None):
     )
 
 
-def class_decorator(interfaces, container, register):
+def class_decorator(interfaces, container, register) -> Any:
     return functools.partial(class_wrapper, interfaces, container, register)
 
 
-def class_wrapper(interfaces, container, register, cls):
+def class_wrapper(
+    interfaces: List[I], container: Container, register: Any, cls: Any
+) -> Any:
     c = container or _container
     if interfaces:
         for i in interfaces:
@@ -156,7 +172,12 @@ def class_wrapper(interfaces, container, register, cls):
     return cls
 
 
-def provide(interfaces, *, scope=None, container=None):
+def provide(
+    interfaces: List[I],
+    *,
+    scope: Optional[Scope] = None,
+    container: Optional[Container] = None
+) -> Callable[[Any], Any]:
     def provide_decorator(func):
         def provide_wrapper(*args, **kwargs):
             s = scope or Scope()
